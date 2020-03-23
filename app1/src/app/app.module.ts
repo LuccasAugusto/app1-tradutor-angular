328feccde71abce25fe658b2160e7adf6a65862c
Luccas Augusto <luccasaugusto@ic.ufal.br>
2020-03-23 15:11:25 -0300

    Creat topo.component and painel.component

--- app1/src/app/app.module.ts
+++ app1/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { PainelComponent } from './painel/painel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    PainelComponent
   ],
   imports: [
     BrowserModule,
