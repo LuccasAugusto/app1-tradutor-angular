bb87074029d9b103058afa04a492777729d51081
Luccas Augusto <luccasaugusto@ic.ufal.br>
2020-03-26 01:48:01 -0300

    string interpolation from instrucao and frases, and  get a event from textarea

--- app1/src/app/painel/painel.component.ts
+++ app1/src/app/painel/painel.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+
+import { Frase } from '../shared/frase.model'
+import { FRASES } from './frases-mock'
 
 @Component({
   selector: 'app-painel',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PainelComponent implements OnInit {
 
-  constructor() { }
+  public frases: Frase[] = FRASES
+  public instrucao: string = 'Traduza a frase:'
+  public resposta:  string
+
+  constructor() { console.log(this.frases) }
 
   ngOnInit(): void {
+  }
+
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    console.log(this.resposta)
   }
 
 }
